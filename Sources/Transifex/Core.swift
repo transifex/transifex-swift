1a6dec65be9d6935c4dd7cba31f23776beaea1f3
Stelios Petrakis <stelabouras@gmail.com>
2021-07-28 17:37:17 +0200

    Bump version
    * Bumps `TXNative.version` to 1.0.0
    * Updates CHANGELOG with the changes implemented for v1.0.0

--- Sources/Transifex/Core.swift
+++ Sources/Transifex/Core.swift
@@ -324,7 +324,7 @@ render '\(stringToRender)' locale code: \(localeCode) params: \(params). Error:
 /// A static class that is the main point of entry for all the functionality of Transifex Native throughout the SDK.
 public final class TXNative : NSObject {
     /// The SDK version
-    internal static let version = "0.5.1"
+    internal static let version = "1.0.0"
     
     /// The filename of the file that holds the translated strings and it's bundled inside the app.
     public static let STRINGS_FILENAME = "txstrings.json"
